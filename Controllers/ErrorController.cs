a157501cb61527ad5fe4a6ed52afb8f27e25a84a
Raul Silveira <raulantsilveira@gmail.com>
2024-11-26 16:43:23 -0300

    feat: create a custom 404 not found webpage

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraStore.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error404")]
+        public IActionResult Error404()
+        {
+            Response.StatusCode = 404; // Define o status HTTP 404
+            return View("Error404"); // Renderiza a View localizada em Views/Shared/Error404.cshtml
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,32 +4,50 @@ using Microsoft.AspNetCore.Identity;
 using UltraStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do banco de dados
 builder.Services.AddDbContext<UltraStoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("UltraStoreContext")));
 
+// Configuração de identidade
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<UltraStoreContext>();
 
-
-// Add services to the container.
+// Adiciona serviços ao container
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+// Middleware para arquivos estáticos
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+// Middleware para páginas de erro personalizadas
+app.UseStatusCodePages(async context =>
+{
+    var response = context.HttpContext.Response;
+
+    // Redireciona 404 para a página Error404
+    if (response.StatusCode == 404)
+    {
+        response.Redirect("/Error404");
+    }
+});
+
+// Configuração das rotas
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
+
 app.Run();
