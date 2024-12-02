221c1526c1fb4312110b9f8a16a09f37fdad29ad
unknown <raulantsilveira@gmail.com>
2024-12-02 16:51:02 -0300

    initialize search box function

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using UltraStore.Data;
+using UltraStore.Models;
 
 namespace UltraStore.Controllers
 {
     public class StoreController : Controller
     {
+        private readonly UltraStoreContext _context;
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SearchGames(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return Json(new { results = new List<Game>() });
+            }
+
+            var games = _context.Game
+                .Where(g => g.Title.Contains(query))
+                .Take(5)
+                .ToList();
+
+            return Json(new { results = games });
         }
     }
 }
