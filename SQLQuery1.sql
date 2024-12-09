410e2765009dadfe81e23415ad661ab921f501c4
Alexandre <alexandre.costa22@outlook.com>
2024-12-09 18:18:45 -0300

    Queries

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Receipt (Cpf, ClientId, SellerId, TotalPrice, CreatedAt)
+VALUES
+('12345678901', 1, 1, 299.99, GETDATE()),
+('98765432109', 2, 2, 199.99, GETDATE()),
+('56789012345', 3, 3, 249.99, GETDATE());
